81941c7ce455dc195bbc1e526c52de17291193f9
Jim Ryan <jim@room118solutions.com>
2013-12-19 15:29:38 -0500

    Ability to insert our routes at runtime (necessary when Rails class caching is disabled, i.e. development)

--- lib/dynamic_paperclip/has_attached_file.rb
+++ lib/dynamic_paperclip/has_attached_file.rb
@@ -41,10 +41,38 @@ module DynamicPaperclip
         action = "generate_#{@klass.name.underscore}"
         default_attachment = @name.to_s.downcase.pluralize
 
-        Rails.application.routes do
-          get url,
-            :to => "DynamicPaperclip::AttachmentStyles##{action}",
-            :defaults => { :attachment => default_attachment }
+        route_options = {
+          :to => "DynamicPaperclip::AttachmentStyles##{action}",
+          :defaults => { :attachment => default_attachment }
+        }
+
+        # If the routes have not been finalized yet, we can just add them as we normally would.
+        # This should always be the case whenever class caching is enabled (i.e. production)
+        # If they have been finalized, we need to hack around a bit to get this added at runtime (primarily for development)
+        if Rails.application.routes.disable_clear_and_finalize
+          Rails.application.routes do
+            get url, route_options
+          end
+        else
+          add_runtime_route! url, route_options
+        end
+      end
+
+      def add_runtime_route!(url, options)
+        begin
+          _routes = Rails.application.routes
+          _routes.disable_clear_and_finalize = true
+          _routes.clear!
+
+          Rails.application.routes_reloader.paths.each{ |path| load(path) }
+
+          _routes.draw do
+            get url, options
+          end
+
+          ActiveSupport.on_load(:action_controller) { _routes.finalize! }
+        ensure
+          _routes.disable_clear_and_finalize = false
         end
       end
   end
