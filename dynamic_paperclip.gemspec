40d82bab50ee6a7621b045d916b659b65db6b1c5
Jim Ryan <jim@room118solutions.com>
2017-04-07 16:21:02 -0400

    Update dummy Rails app

--- dynamic_paperclip.gemspec
+++ dynamic_paperclip.gemspec
@@ -26,5 +26,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'sqlite3'
   s.add_development_dependency 'shoulda'
   s.add_development_dependency 'mocha'
-  s.add_development_dependency "rails", "> 3.2"
+  s.add_development_dependency "rails", "~> 4.2.8"
 end
--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -1,7 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '959ab32349c805bdfd234602c492d9b06bcb9277a650fd6b9fe0670cc1f78ce67a133b90a4ce193401870b30a2194f23652b2a8f41dd50d92cd6a01222325f7a'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dummy::Application.config.secret_key_base = '1308728b934efe940ce301800d4c83287bf7931b6ac3b9bbf68ce053b9faacde9de2052fe243fb5fd6d5e361b53ae353146c4469e59a51c4bdccafdcb068350d'
