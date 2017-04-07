01f0ac387abe7064d1ed4488fe3ad52b0485403c
Jim Ryan <jim@room118solutions.com>
2017-04-07 16:31:16 -0400

    Only call do_not_validate_attachment_file_type for Paperclip 4 and up

--- test/dummy/app/models/photo.rb
+++ test/dummy/app/models/photo.rb
@@ -1,4 +1,7 @@
 class Photo < ActiveRecord::Base
   has_dynamic_attached_file :image, styles: { thumb: '100x100#' }
-  do_not_validate_attachment_file_type :image
+
+  if Gem::Version.new(Paperclip::VERSION) >= Gem::Version.new('4')
+    do_not_validate_attachment_file_type :image
+  end
 end
